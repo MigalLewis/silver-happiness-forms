49ce64c2fb7d641b0d499e993e09abbcbe5517b3
Migal Lewis <migal.lewis@standardbank.co.za>
2017-04-02 17:23:10 +0200

    Added funding page and fix for table adding

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,25 +1,24 @@
 app.controller('businessController', function ($scope) {
   $scope.headingTitle = "User List";
-  $scope.addressDetails = [
-    {
-      'street1': 'barbara rd',
+  $scope.addressDetails = [{
+      'street1': '',
       'street2': '',
       'street3': '',
-      'suburb': 'woodmere',
-      'postalcode': '1401',
-      'city': 'Germiston',
-      'country': 'South Africa'
-    }
-  ];
-  $scope.addNew = function (details) {
+      'suburb': '',
+      'postalcode': '',
+      'city': '',
+      'country': ''
+    }];
+
+  $scope.addNew = function () {
     $scope.addressDetails.push({
-      'street1': details.street1,
-      'street2': details.street2,
-      'street3': details.street3,
-      'suburb': details.suburb,
-      'postalcode': details.postalcode,
-      'city': details.city,
-      'country': details.country
+      'street1': '',
+      'street2': '',
+      'street3': '',
+      'suburb': '',
+      'postalcode': '',
+      'city': '',
+      'country': ''
     });
   };
 
@@ -44,77 +43,146 @@ app.controller('businessController', function ($scope) {
       addressDetails.selected = $scope.selectedAll;
     });
   };
+  $scope.save = function () {
+
+  };
 });
-app.controller("memberController",  function ($scope) {
-    $scope.titlelist = ['Mr', 'Mrs', 'Miss'];
-    $scope.ethnitylist = ['Black', 'Coulard', 'Indian', 'White'];
-    $scope.personalDetails = [
-      {
-        'title': "Mr",
-        'fname': 'Muhammed',
-        'lname': 'Shanid',
-        'idnumber': '9007055078089',
-        'ethnity': 'Indian',
-        'cnumber': '0795028226',
-        'email': 'shanid@shanid.com'
-      },
-      {
-        'title': "Mr",
-        'fname': 'John',
-        'lname': 'Abraham',
-        'ethnity': 'White',
-        'idnumber': '9007055078089',
-        'cnumber': '0795028226',
-        'email': 'john@john.com'
-      },
-      {
-        'title': "Mr",
-        'fname': 'Roy',
-        'lname': 'Mathew',
-        'ethnity': 'White',
-        'idnumber': '9007055078089',
-        'cnumber': '0795028226',
-        'email': 'roy@roy.com'
-      }];
+app.controller("memberController", function ($scope) {
+  $scope.titlelist = ['Mr', 'Mrs', 'Miss'];
+  $scope.ethnitylist = ['Black', 'Coulard', 'Indian', 'White'];
+  $scope.personalDetails = [{
+      'title': '',
+      'fname': '',
+      'lname': '',
+      'idnumber': '',
+      'ethnity': '',
+      'cnumber': '',
+      'email': ''
+    }];
 
-    $scope.addNew = function (details) {
-      $scope.personalDetails.push({
-        'title': details.title,
-        'fname': details.fname,
-        'lname': details.lname,
-        'idnumber': details.idnumber,
-        'ethnity': details.ethnity,
-        'cnumber': details.cnumber,
-        'email': details.email
-      });
-      $scope.PD = {};
-    };
+  $scope.addNew = function () {
+    $scope.personalDetails.push({
+      'title': '',
+      'fname': '',
+      'lname': '',
+      'idnumber': '',
+      'ethnity': '',
+      'cnumber': '',
+      'email': ''
+    });
+  };
 
-    $scope.remove = function () {
-      var newDataList = [];
-      $scope.selectedAll = false;
-      angular.forEach($scope.personalDetails, function (selected) {
-        if (!selected.selected) {
-          newDataList.push(selected);
-        }
-      });
-      $scope.personalDetails = newDataList;
-    };
-
-    $scope.checkAll = function () {
-      if (!$scope.selectedAll) {
-        $scope.selectedAll = true;
-      } else {
-        $scope.selectedAll = false;
+  $scope.remove = function () {
+    var newDataList = [];
+    $scope.selectedAll = false;
+    angular.forEach($scope.personalDetails, function (selected) {
+      if (!selected.selected) {
+        newDataList.push(selected);
       }
-      angular.forEach($scope.personalDetails, function (personalDetails) {
-        personalDetails.selected = $scope.selectedAll;
-      });
-    };
-  });
+    });
+    $scope.personalDetails = newDataList;
+  };
+
+  $scope.checkAll = function () {
+    if (!$scope.selectedAll) {
+      $scope.selectedAll = true;
+    } else {
+      $scope.selectedAll = false;
+    }
+    angular.forEach($scope.personalDetails, function (personalDetails) {
+      personalDetails.selected = $scope.selectedAll;
+    });
+  };
+
+  $scope.save = function () {
+
+  };
+});
 
 app.controller('fundingController', function ($scope) {
   $scope.headingTitle = "User List";
+  $scope.financeRequired = '';
+  $scope.financeApplied = '';
+  $scope.statusList = ['Approved', 'Regretted', 'In process'];
+  $scope.fundingItemTypeList = ['Equipement', 'System Enhancement', 'System Developement', 'Commisioning', 'Instalations', 'Other'];
+  $scope.sponsors = [{
+      'name': "",
+      'status': '',
+      'amount': '',
+      'comment': ''
+    }];
+  $scope.fundintItems = [{
+      'name': "",
+      'description': '',
+      'type': '',
+      'amount': ''
+    }];
+
+  $scope.addNewSponser = function () {
+    $scope.sponsors.push({
+      'name': "",
+      'status': '',
+      'amount': '',
+      'comment': ''
+    });
+  };
+
+  $scope.removeSponser = function () {
+    var newDataList = [];
+    $scope.selectedAll = false;
+    angular.forEach($scope.sponsors, function (selected) {
+      if (!selected.selected) {
+        newDataList.push(selected);
+      }
+    });
+    $scope.sponsors = newDataList;
+  };
+
+  $scope.checkAllSponser = function () {
+    if (!$scope.selectedAll) {
+      $scope.selectedAll = true;
+    } else {
+      $scope.selectedAll = false;
+    }
+    angular.forEach($scope.sponsors, function (sponsors) {
+      sponsors.selected = $scope.selectedAll;
+    });
+  };
+
+  $scope.addNewFundingItem = function () {
+    $scope.fundintItems.push({
+      'name': "",
+      'description': '',
+      'type': '',
+      'amount': ''
+    });
+  };
+
+  $scope.removeFundingItem = function () {
+    var newDataList = [];
+    $scope.selectedAll = false;
+    angular.forEach($scope.fundintItems, function (selected) {
+      if (!selected.selected) {
+        newDataList.push(selected);
+      }
+    });
+    $scope.fundintItems = newDataList;
+  };
+
+  $scope.checkAllFundingItem = function () {
+    if (!$scope.selectedAll) {
+      $scope.selectedAll = true;
+    } else {
+      $scope.selectedAll = false;
+    }
+    angular.forEach($scope.fundintItems, function (fundintItems) {
+      fundintItems.selected = $scope.selectedAll;
+    });
+  };
+
+  $scope.save = function () {
+
+  };
 });
 
 app.controller('projectsController', function ($scope) {
